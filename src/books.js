1cd23a8c22aa5c70828b4c85be125ab023a6c2c6
Anuj Bhandari <anujbhandari.7676@gmail.com>
2021-02-22 20:28:28 +0530

    second commit

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const data = [
+    {
+      id: 1,
+      img: 'https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg',
+      title: 'I love you to the Moon and back',
+      author: 'Amelia Hepworth'
+    },
+  
+    {
+      id: 2,
+      img: 'https://images-eu.ssl-images-amazon.com/images/I/81s6DUyQCZL._AC_UL200_SR200,200_.jpg',
+      title: 'Think Like a Monk',
+      author: 'Jay Shetty'
+    }
+  ]
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom'; 
+import './index.css';
+
+import { data } from './books'; 
+import SpecificBook from './Book';
+import {greeting} from './testing/testing'
+
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newNames);
 
 function BookList(){
+  console.log(greeting);
   return (
-    <section>
-      this is a booklist
+    <section className='booklist'> 
+      {data.map((book) => {
+        const { img, title, author } = book;
+        return (
+          <SpecificBook key={book.id} {...book}></SpecificBook>
+        )
+      })}
     </section>
   );
 }
 
-const Book = () => {
-  return <article>this is a book</article>
-
-}
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
